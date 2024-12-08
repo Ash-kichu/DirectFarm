313b97fb2375b2b6b3619ea3725f072cf7b13050
ashik <ashikps957@gmail.com>
2024-12-08 16:44:34 +0000

    changes

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Home')
+    path('', views.home, name='Home'),
+    path('shop/', views.shop, name='Shop'),
+    path('about/', views.about, name='About'),
+    path('farmers/', views.farmers, name='Farmers'),
+    path('contact/', views.contact, name='Contact'),
+    path('account/', views.account, name='Account'),
+    path('cart/', views.cart, name='Cart'),
+    path('signup/', views.signup, name='Sign Up'),
+    path('login/', views.login, name='Login'),
 ]
